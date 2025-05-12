b3c9691d01266186ce98d8c7ac78cc70e73e54ef
panayiotis <panayiotismay@outlook.com>
2025-05-12 11:34:09 +0300

    Updated Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,18 @@
 #include "banker.h"
 
-int main() {}
+int main() {
+
+        int n, m;
+        std::cout << "Enter the number of threads in the system: ";
+        std::cin >> n;
+        std::cout << "Enter the number of resource types in the system: ";
+        std::cin >> m;
+
+        SystemState system(n, m);
+        system.inputState();
+        system.menu();
+
+        return 0;
+    
+
+}
