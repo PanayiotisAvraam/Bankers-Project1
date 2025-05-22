11522f7091c6d4352768b42633a02019171dd3b3
panayiotis <panayiotismay@outlook.com>
2025-05-22 11:29:33 +0300

    inserted code for main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,17 +2,17 @@
 
 int main() {
 
-        int n, m;
-        std::cout << "Enter the number of threads in the system: ";
-        std::cin >> n;
-        std::cout << "Enter the number of resource types in the system: ";
-        std::cin >> m;
+    int n, m;
+    std::cout << "Enter the number of threads in the system: ";
+    std::cin >> n;
+    std::cout << "Enter the number of resource types in the system: ";
+    std::cin >> m;
 
-        SystemState system(n, m);
-        system.inputState();
-        system.menu();
+    SystemState system(n, m);
+    system.inputState();
+    system.menu();
 
-        return 0;
-    
+    return 0;
+
 
 }
